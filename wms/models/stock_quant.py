f558172e990cfde968a03770f327fb8a06fe3733
huudm-teko <huu.dm@teko.vn>
2019-05-02 09:34:51 +0700

    update stock quant descriptions

--- wms/models/stock_quant.py
+++ wms/models/stock_quant.py
@@ -11,26 +11,26 @@ _logger = logging.getLogger('api')
 class StockQuantSchema:
     stock_quant_req = {
         'products': fields.String(required=True, description='List sku'),
-        'branches': fields.String(required=False, description='Chi nhánh'),
-        'warehouses': fields.String(required=False, description='Mã Kho'),
-        'locations': fields.String(required=False, description='Địa điểm'),
-        'regions': fields.Integer(required=False, description='Vùng / miền'),
+        'branches': fields.String(required=False, description='Branches'),
+        'warehouses': fields.String(required=False, description='Warehouses code'),
+        'locations': fields.String(required=False, description='Locations'),
+        'regions': fields.Integer(required=False, description='Regions'),
     }
 
     stock_quant_item = {
         # 'sku': fields.String(required=True, description='product sku'),
-        'location': fields.String(required=False, description='Chi nhánh'),
-        'warehouse': fields.String(required=False, description='Mã Kho'),
-        'reserved': fields.Float(required=False, description='Số lượng đã giữ'),
-        'incoming': fields.Integer(required=False, description='Số lượng sắp nhập kho'),
-        'storeCode': fields.String(required=False, description='loại khu vực Asia'),
-        'outgoing': fields.Integer(required=False, description='Số lượng sắp xuất kho'),
+        'location': fields.String(required=False, description='Branch'),
+        'warehouse': fields.String(required=False, description='Warehouse code'),
+        'reserved': fields.Float(required=False, description='Stock held quantity'),
+        'incoming': fields.Integer(required=False, description='Quantity about to enter stock'),
+        'storeCode': fields.String(required=False, description='Asia region type'),
+        'outgoing': fields.Integer(required=False, description='Stock outgoing quantity'),
         'available': fields.Float(required=False,
-                                  description='Số lượng tồn kho thực tế - số đã giữ hàng = on_hand- reserved',
+                                  description='Available quantity = Actual inventory number - stock held = onHand- reserved',
                                   ),
         'forecast': fields.Float(required=False, description='No description'),
-        'onHand': fields.Float(required=False, description='Số lượng thực tế đang có trong kho'),
-        'productBizType': fields.String(required=False, description='Loại hình kinh doanh sản phẩm'),
+        'onHand': fields.Float(required=False, description='Actual quantity in stock'),
+        'productBizType': fields.String(required=False, description='Type of product business'),
     }
     # Remove some fields for stock_quant_item_min
     stock_quant_item_min = stock_quant_item.copy()
